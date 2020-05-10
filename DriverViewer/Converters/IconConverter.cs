f42f312a8a16e30184bdf764d67416617e9bed0c
gdeming <demingry@sina.com>
2020-05-10 23:33:05 +0800

    Added

---
+++ DriverViewer/Converters/IconConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using GalaSoft.MvvmLight;
+
+namespace DriverViewer.Converters
+{
+    class IconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+            if (path == null) return null;
+
+            var name = MainWindow.GetFileFolderName(path);
+
+            var Icon = "\xe666";
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Icon = "\xe72a";
+            }else if(new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                Icon = "\xe60c";
+            }
+
+            return Icon;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DriverViewer/MainWindow.xaml.cs
+++ DriverViewer/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DriverViewer
 {
@@ -24,5 +18,93 @@ namespace DriverViewer
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drives in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                item.Tag = drives;
+                item.Header = drives;
+                item.Items.Add(null);
+                item.Expanded += Folder_Expanded;   //为展开操作订阅事件
+                FolderViewer.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count != 1 || item.Items[0] != null) return;
+            item.Items.Clear();
+
+            #region 尝试获取Drive下的文件夹
+            var dictionaries = new List<string>();
+            try
+            {
+                var dirs = Directory.GetDirectories((string)item.Tag);
+                if (dirs.Length > 0) dictionaries.AddRange(dirs);
+            }
+            catch
+            {
+                // ignored
+            }
+
+
+            dictionaries.ForEach(dic =>
+            {
+                var subitem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(dic),
+                    Tag = dic
+                };
+
+                subitem.Items.Add(null);
+                subitem.Expanded += Folder_Expanded;
+
+                item.Items.Add(subitem);
+            });
+            #endregion
+
+
+            #region 获取文件
+            var files = new List<string>();
+            try
+            {
+                var fs = Directory.GetFiles((string)item.Tag);
+                if (fs.Length > 0) files.AddRange(fs);
+            }
+            catch
+            {
+                // ignored
+            }
+
+
+            files.ForEach(filepath =>
+            {
+                var subitem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(filepath),
+                    Tag = filepath
+                };
+
+                item.Items.Add(subitem);
+            });
+            #endregion
+        }
+
+
+        #region 获取文件的名字GetFileFolderName()
+        public static string GetFileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path)) return string.Empty;
+            var normalizedPath = path.Replace('/', '\\');
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+            if (lastIndex <= 0) return path;
+
+            return path.Substring(lastIndex + 1);
+        }
+        #endregion
     }
 }
